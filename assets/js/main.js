5c421783d5a8e90521f1397c8184e9973d9eaafc
federico-casu <federico.casu1101@gmail.com>
2024-01-08 18:19:55 +0100

    fix: fixed ageRange initialization

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,13 @@ let generateButton = document.getElementById("generate");
 generateButton.addEventListener('click', function(){
     let name = document.getElementById("name").value;
     let km = Number(document.getElementById("km").value);
-    let ageRange = document.getElementById("age").value;
+    let ageRange = Number(document.getElementById("age").value);
     let ticketPrice = km * kmCost;
 
     document.getElementById("passengerName").innerHTML = name;
     document.getElementById("carriage").innerHTML = Math.floor(Math.random()* 10);
     document.getElementById("cpCode").innerHTML = Math.floor(Math.random()* 100000);
+
 
     switch (ageRange) {
         case 1:
