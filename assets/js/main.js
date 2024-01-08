30249e92073d431ce70e50b97096957ae9f5ca1c
federico-casu <federico.casu1101@gmail.com>
2024-01-08 18:08:05 +0100

    feat: added ticket details after being generated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-
+document.body.style.backgroundColor = "#b91550";
 
 const kmCost = 0.21;
 let generateButton = document.getElementById("generate");
@@ -10,21 +10,25 @@ generateButton.addEventListener('click', function(){
     let ageRange = document.getElementById("age").value;
     let ticketPrice = km * kmCost;
 
+    document.getElementById("passengerName").innerHTML = name;
+    document.getElementById("carriage").innerHTML = Math.floor(Math.random()* 10);
+    document.getElementById("cpCode").innerHTML = Math.floor(Math.random()* 100000);
+
     switch (ageRange) {
         case 1:
             ticketPrice -= ((ticketPrice) * 0.2);
-            document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+            document.getElementById("offer").innerHTML = "Biglietto Ridotto";
             break;
         case 3:
             ticketPrice -= ((ticketPrice) * 0.4);
-            document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+            document.getElementById("offer").innerHTML = "Biglietto Over 65";
             break;
         default:
-            document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+            document.getElementById("offer").innerHTML = "Biglietto Standard";
             break;
     }
 
-    console.log(name, km, ageRange)
+    document.getElementById("priceOnTicket").innerHTML = `${ticketPrice.toFixed(2)} €`;
 
-    console.log(`Il costo del biglietto è: ${ticketPrice.toFixed(2)}`);
+    document.getElementById("ticket").classList.remove("d-none");
 })
